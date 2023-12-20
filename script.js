4a96430cb6383964b3459af0b29498cb2c1e7951
DESKTOP-DHUHJ2H\timon <timongaleazzi@gmail.com>
2023-12-20 19:11:03 +0100

    some stuff

--- script.js
+++ script.js
@@ -24,14 +24,15 @@ function setupInfiniteScroll() {
   document.addEventListener('DOMContentLoaded', setupInfiniteScroll);
   
 
-// Radar chart
-const skillData = {
+  const skillData = {
     labels: ['Web Development', 'Graphic Design', 'Project Management', 'Communication', 'Problem Solving'],
     datasets: [{
+        label: 'Skills',
         data: [8, 7, 6, 9, 8],
         backgroundColor: 'rgba(75, 192, 192, 0.2)',
         borderColor: 'rgba(75, 192, 192, 1)',
         borderWidth: 2,
+        pointBackgroundColor: 'rgba(75, 192, 192, 1)'
     }],
 };
 
@@ -47,16 +48,52 @@ if (!ctx) {
             scales: {
                 r: {
                     angleLines: {
-                        display: false,
+                        color: 'rgba(255, 255, 255, 0.3)' // Light gray angle lines
+                    },
+                    grid: {
+                        color: 'rgba(255, 255, 255, 0.3)' // Light gray grid lines
+                    },
+                    pointLabels: {
+                        font: {
+                            size: 14, // Adjust font size as needed
+                            family: 'Arial', // Use a consistent font
+                        },
+                        color: '#FFFFFF' // White font color for labels
+                    },
+                    ticks: {
+                        color: 'rgba(255, 255, 255, 0.6)', // Light gray tick labels
+                        backdropColor: 'transparent', // Transparent background for tick labels
+                        stepSize: 2,
                     },
                     suggestedMin: 0, // Suggest a minimum value of 0
                     suggestedMax: 10, // Suggest a maximum value of 10
-                    ticks: {
-                        stepSize: 1,
-                        callback: function (value) {
-                            return value; // Show all tick values
+                }
+            },
+            plugins: {
+                tooltip: {
+                    usePointStyle: true,
+                    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+                    titleFont: {
+                        size: 16,
+                        family: 'Arial',
+                    },
+                    bodyFont: {
+                        size: 14,
+                        family: 'Arial',
+                    },
+                    titleColor: '#FFFFFF',
+                    bodyColor: '#FFFFFF',
+                    borderColor: 'rgba(75, 192, 192, 0.4)',
+                    borderWidth: 1
+                },
+                legend: {
+                    labels: {
+                        color: 'rgba(255, 255, 255, 0.7)',
+                        font: {
+                            family: 'Arial',
                         },
                     },
+                    position: 'top'
                 },
             },
             responsive: true,
@@ -67,34 +104,75 @@ if (!ctx) {
 
 
 
-// Contact form toggle
-const contactButton = document.getElementById('showContactForm');
-const contactForm = document.querySelector('.contact-form');
+function setupInfiniteScroll() {
+    const testimonialContainer = document.querySelector('.testimonial-container');
+    const testimonials = Array.from(testimonialContainer.children);
+    const totalWidth = testimonials.reduce((total, elem) => total + elem.offsetWidth + 10, 0);
+  
+    testimonialContainer.append(...testimonials.map(testimonial => testimonial.cloneNode(true)));
+  
+    let currentScrollPosition = 0;
+    let scrollInterval;
+    const step = 2;
+  
+    function scrollTestimonials() {
+        currentScrollPosition += step;
+        if (currentScrollPosition >= totalWidth) {
+            currentScrollPosition = 0;
+        }
+        testimonialContainer.scrollLeft = currentScrollPosition;
+    }
+  
+    function startScrolling() {
+        scrollInterval = setInterval(scrollTestimonials, 20);
+    }
 
-if (contactButton && contactForm) {
-    contactButton.addEventListener('click', function () {
-        contactForm.classList.toggle('hidden');
-    });
-} else {
-    console.error('Could not find the contact form elements.');
+    function stopScrolling() {
+        clearInterval(scrollInterval);
+    }
+
+    testimonialContainer.addEventListener('mouseover', stopScrolling);
+    testimonialContainer.addEventListener('mouseout', startScrolling);
+
+    startScrolling();
 }
 
-// Timeline details
-const timeline = document.getElementById('timeline');
-const detailsContainer = document.getElementById('timeline-details');
-const closeDetailsButton = document.getElementById('close-details');
+document.addEventListener('DOMContentLoaded', setupInfiniteScroll);
 
-if (timeline && detailsContainer && closeDetailsButton) {
-    timeline.querySelectorAll('.timeline-event').forEach(event => {
-        event.addEventListener('click', () => {
-            detailsContainer.innerHTML = event.querySelector('.timeline-content').innerHTML;
-            detailsContainer.style.display = 'block';
-        });
-    });
 
-    closeDetailsButton.addEventListener('click', () => {
-        detailsContainer.style.display = 'none';
-    });
-} else {
-    console.error('Could not find required timeline elements.');
+// Function to show event details in a modal
+function showEventDetails(event) {
+    // Assuming your event details are in '.timeline-content' inside '.timeline-event'
+    const eventDetails = event.querySelector('.timeline-content').innerHTML;
+    const detailsModal = document.getElementById('event-details-modal');
+
+    // Update modal content and display it
+    if (detailsModal) {
+        detailsModal.innerHTML = eventDetails;
+        detailsModal.style.display = 'block';
+    }
 }
+
+// Function to hide the event details
+function hideEventDetails() {
+    const detailsModal = document.getElementById('event-details-modal');
+    if (detailsModal) {
+        detailsModal.style.display = 'none';
+    }
+}
+
+
+// Contact form show/hide functionality
+document.addEventListener('DOMContentLoaded', () => {
+    setupInfiniteScroll();
+  
+    const contactButton = document.getElementById('showContactForm');
+    const contactFormContainer = document.getElementById('contactFormContainer');
+  
+    contactButton.addEventListener('click', () => {
+      contactFormContainer.classList.toggle('active');
+    });
+  
+    // You should also setup your radar chart here if it is not done already
+    // new Chart(ctx, { type: 'radar', data: skillData, options: { ... } });
+  });
